f23117fe50e1ae323c93c7e04b1166c156308957
matheus-dev-fs <matheusmks10@gmail.com>
2026-03-02 15:51:45 -0300

    feat: Initialize IMC calculator project with basic structure
    - Create initial React project structure
    - Add App component with layout for IMC calculator
    - Add CSS module for basic styling and layout
    - Include powered image asset in header

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { JSX } from "react";
+import styles from "./App.module.css";
+import poweredImage from "./assets/powered.png";
+
+const App = (): JSX.Element => {
+    return (
+        <div className={styles.main}>
+            <header>
+                <div className={styles.headerContainer}>
+                    <img src={poweredImage} alt="Powered by" width={150} />
+                </div>
+            </header>
+            <div className={styles.container}>
+                <div className={styles.leftSide}>
+                    ...
+                </div>
+                <div className={styles.rightSide}>
+                    ...
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
